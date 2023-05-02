8e5d5f0b80bb95df43fa4d749e8676296e145ce4
Hugolino B. Guimarães <hugolino2609@gmail.com>
2023-05-02 15:04:56 -0300

    Update ulils.js

--- asset/js/ulils.js
+++ asset/js/ulils.js
@@ -9,9 +9,31 @@ function calculateIMC({ weight, height }) {
 }
 
 function displayResultMessage(result) {
+  const weightRating = document.querySelector("p")
   const message = `Seu IMC é de ${result}`
 
   Modal.message.innerText = message
+
+  let underWeight = result < 18.5
+  let normalWeight = result >= 18.5 && result <= 24.99
+  let preObesity = result >= 25.0 && result <= 29.99
+  let GradeOneObesity = result >= 30.0 && result <= 34.99
+  let GradeTwoObesity = result >= 35.0 && result <= 39.99
+
+  if (underWeight) {
+    weightRating.innerText = "Abaixo do Peso"
+  } else if (normalWeight) {
+    weightRating.innerText = "Peso Normal"
+  } else if (preObesity) {
+    weightRating.innerText = "Pré-obesidade"
+  } else if (GradeOneObesity) {
+    weightRating.innerText = "Obesidade Grau 1"
+  } else if (GradeTwoObesity) {
+    weightRating.innerText = "Obesidade Grau 2"
+  } else {
+    weightRating.innerText = "Obesidade Extrema"
+  }
+
   Modal.open()
 }
 
